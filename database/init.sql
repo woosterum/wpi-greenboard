53112879d1803cedefc6bf5ebfb7b2a32639fb70
woosterum <84195597+woosterum@users.noreply.github.com>
2025-10-28 16:55:15 -0400

    update schema

--- database/init.sql
+++ database/init.sql
@@ -2,12 +2,12 @@ CREATE DATABASE greenboard;
 
 CREATE TABLE carriers (
     carrier_id SERIAL PRIMARY KEY,
-    carrier_name VARCHAR(20) UNIQUE NOT NULL
+    carrier_name VARCHAR(20) NOT NULL
 );
 
 INSERT INTO carriers (carrier_name) VALUES ('Other');
 
-CREATE TABLE emmissions (
+CREATE TABLE emissions (
     service_type VARCHAR(50) PRIMARY KEY,
     emission_factor FLOAT
 );
@@ -18,28 +18,52 @@ CREATE TABLE persons (
     last_name VARCHAR(50),
     is_student BOOLEAN,
     is_mailroom_worker BOOLEAN,
-    box_number VARCHAR(255),
+    box_number VARCHAR(5),
     class_year INT, -- If > 4 => Graduate Student
-    supervisor_id CHAR(9) NULL REFERENCES persons(wpi_id)
-);
-
-
-CREATE TABLE packages (
-    package_id SERIAL PRIMARY KEY,
-    carrier_id INT REFERENCES carriers(carrier_id),
-    tracking_number VARCHAR(255) NOT NULL,
-    recipient_id CHAR(9) NOT NULL REFERENCES persons(wpi_id),
-    service_type VARCHAR(50) REFERENCES emmissions(service_type),
-    date_shipped TIMESTAMP,
-    total_emissions_kg FLOAT,
-    distance_traveled FLOAT
+    supervisor_id CHAR(9) NULL,
+    CONSTRAINT supervisor_fk
+        FOREIGN KEY (supervisor_id)
+        REFERENCES persons(wpi_id)
+        ON DELETE SET NULL  -- if supervisor is deleted, set supervisor_id to null
+        ON UPDATE CASCADE
 );
 
 -- Indicates department or major affiliation
 CREATE TABLE departments (
-    person_id CHAR(9) REFERENCES persons(wpi_id),
-    department_name VARCHAR(100),
-    PRIMARY KEY (person_id, department_name)
+    person_id CHAR(9) NOT NULL,
+    department_name VARCHAR(100) NOT NULL,
+    PRIMARY KEY (person_id, department_name),
+    CONSTRAINT departments_fk
+        FOREIGN KEY (person_id)
+        REFERENCES persons(wpi_id)
+        ON DELETE CASCADE  -- delete departments when person is deleted
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE packages (
+    package_id SERIAL PRIMARY KEY,
+    carrier_id INT NULL,
+    recipient_id CHAR(9) NULL,
+    tracking_number VARCHAR(255) NOT NULL,
+    service_type VARCHAR(50) NULL,
+    date_shipped TIMESTAMP,
+    total_emissions_kg FLOAT,
+    distance_traveled FLOAT,
+    CONSTRAINT packages_recipient_fk
+        FOREIGN KEY (recipient_id)
+        REFERENCES persons(wpi_id)
+        ON DELETE SET NULL  -- keep package if an recipient row is deleted
+        ON UPDATE CASCADE,
+    CONSTRAINT carrier_id_fk
+        FOREIGN KEY (carrier_id)
+        REFERENCES carriers(carrier_id)
+        ON DELETE RESTRICT  -- cannot delete carrier if package references it
+        ON UPDATE CASCADE,
+    CONSTRAINT service_type_fk
+        FOREIGN KEY (service_type)
+        REFERENCES emissions(service_type)
+        ON DELETE SET NULL  -- keep package if an emissions row is deleted
+        ON UPDATE CASCADE
 );
 
 CREATE TYPE transaction_types AS ENUM ('delivered', 'stored', 'routed');
@@ -48,9 +72,18 @@ CREATE TABLE transactions (
     transaction_id SERIAL PRIMARY KEY,
     date TIMESTAMP,
     transaction_type TRANSACTION_TYPES,
-    locker VARCHAR(255),
-    location VARCHAR(255),
-    package_id INT NOT NULL REFERENCES packages(package_id),
-    worker_id CHAR(9) NOT NULL REFERENCES persons(wpi_id)
+    locker VARCHAR(20),
+    location VARCHAR(20),
+    package_id INT NOT NULL,
+    worker_id CHAR(9) NULL,
+    CONSTRAINT transactions_package_fk
+        FOREIGN KEY (package_id)
+        REFERENCES packages(package_id)
+        ON DELETE RESTRICT  -- block deleting a package if transactions exist
+        ON UPDATE CASCADE,
+    CONSTRAINT transactions_worker_fk
+        FOREIGN KEY (worker_id)
+        REFERENCES persons(wpi_id)
+        ON DELETE SET NULL  -- keep transaction if worker is deleted
+        ON UPDATE CASCADE
 );
-
